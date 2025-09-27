78a1687b0543b26517a9cb45ad6d3f54ab5dd665
Ultras <UltrasMiro@gmail.com>
2025-09-27 10:22:16 +0300

    ConsoleMenu.h created Menu logic completed FileS.h logic completed Comments added

---
+++ ConsoleMenu.h
@@ -0,0 +1,138 @@
+#ifndef CONSOLEMENU_H
+#define CONSOLEMENU_H
+
+#include <iostream>
+#include "FileS.h"
+
+using namespace std;
+
+class ConsoleMenu {
+private:
+    FileRepository &repository;
+
+public:
+    ConsoleMenu(FileRepository &repo) : repository(repo) {}
+
+    void Show() {
+        int choice;
+        do {
+            cout << "\n====== Menu ======\n";
+            cout << "1. Add Student\n";
+            cout << "2. Add Librarian\n";
+            cout << "3. Add Software Developer\n";
+            cout << "4. Search by surname\n";
+            cout << "5. Save to file\n";
+            cout << "6. Read file\n";
+            cout << "7. Search students with ideal weight\n";
+            cout << "8. Delete by surname\n";
+            cout << "0. Exit\n";
+            cout << "Enter your choice: \n";
+            cin >> choice;
+            cin.ignore();
+
+            switch (choice) {
+                case 1:      // Починає цикл для створення студента
+                    AddStudent();
+                    break;
+                case 2:      // Починає цикл для створення бібліотекаря
+                    AddLibrarian();
+                    break;
+                case 3:      // Починає цикл для створення програміста
+                    AddSoftwareDeveloper();
+                    break;
+                case 4: {    // Завантажує дані з файлу і починає пошук всієї інфи за прізвищем
+                    repository.LoadAll("database.txt");
+                    string surname;
+                    cout << "Enter surname to search: ";
+                    getline(cin, surname);
+                    repository.SearchBySurname(surname);
+                    break;
+                }
+                case 5:      // Зберігає всю інфу у файл
+                    repository.SaveAll("database.txt");
+                    cout << "Saving data.\n";
+                    break;
+                case 6:      // Зчитує інфу з файлу і виводить її в консоль
+                    repository.LoadAll("database.txt");
+                    repository.PrintAll();
+                    break;
+                case 7: {    // Виводить інфу про студентів з ідеальною вагою
+                    repository.LoadAll("database.txt");
+                    repository.PrintIdealWeightStudents();
+                    break;
+                }
+                case 8: {    // Шукає прізвище в файлі і видаляє всю інфу про нього
+                    repository.LoadAll("database.txt");
+                    string surname;
+                    cout << "Enter surname to delete: ";
+                    getline(cin, surname);
+                    repository.DeleteBySurname(surname);
+                    repository.SaveAll("database.txt");
+                    break;
+                }
+                case 0:      // Вихід
+                    cout << "Exiting...\n";
+                    break;
+                default:
+                    cout << "Wrong choice!\n";
+            }
+        } while (choice != 0);
+    }
+
+    // Виводить блок тексту і дозволяє вводити інфу при створенні студента
+    void AddStudent() {
+        string fname, lname, studentId, passport;
+        int height, weight;
+
+        cout << "Enter name: ";
+        getline(cin, fname);
+        cout << "Enter surname: ";
+        getline(cin, lname);
+        cout << "Enter height: ";
+        cin >> height;
+        cout << "Enter weight: ";
+        cin >> weight;
+        cin.ignore();
+        cout << "Enter student id: ";
+        getline(cin, studentId);
+        cout << "Enter id: ";
+        getline(cin, passport);
+
+        repository.Add(new Student(fname, lname, height, weight, studentId, passport));
+        cout << "Student has been added.\n";
+    }
+
+    // Виводить блок тексту і дозволяє вводити інфу при створенні бібліотекаря
+    void AddLibrarian() {
+        string fname, lname, employeeId;
+
+        cout << "Enter name: ";
+        getline(cin, fname);
+        cout << "Enter surname: ";
+        getline(cin, lname);
+        cout << "Enter employee Id: ";
+        getline(cin, employeeId);
+
+        repository.Add(new Librarian(fname, lname, employeeId));
+        cout << "Librarian has been added.\n";
+    }
+
+    // Виводить блок тексту і дозволяє вводити інфу при створенні програміста
+    void AddSoftwareDeveloper() {
+        string fname, lname, devId, lang;
+
+        cout << "Enter name: ";
+        getline(cin, fname);
+        cout << "Enter surname: ";
+        getline(cin, lname);
+        cout << "Enter developer's Id: ";
+        getline(cin, devId);
+        cout << "Enter programming language: ";
+        getline(cin, lang);
+
+        repository.Add(new SoftwareDeveloper(fname, lname, devId, lang));
+        cout << "Software developer has been added.\n";
+    }
+};
+
+#endif
--- FileS.h
+++ FileS.h
@@ -5,6 +5,7 @@
 #include "SoftwareDev.h"
 #include <string>
 #include <fstream>
+#include <regex>
 #include <iostream>
 using namespace std;
 
@@ -14,19 +15,148 @@ private:
     int count_ = 0;
 
 public:
-    /
+    //
     bool Add(Record* r) {
         if (count_ >= 100) return false;
         records_[count_++] = r;
         return true;
     }
 
-
+    // Виводить всю інфу з файлу
     void PrintAll() const {
         for (int i = 0; i < count_; ++i) {
             cout << records_[i]->describe() << endl;
         }
     }
 
+    // Пошук по фамілії
+    void SearchBySurname(const string& surname) const {
+        bool found = false;
+        for (int i = 0; i < count_; ++i) {
+            string lname;
+            if (Student* s = dynamic_cast<Student*>(records_[i])) lname = s->lastName();
+            else if (Librarian* l = dynamic_cast<Librarian*>(records_[i])) lname = l->lastName();
+            else if (SoftwareDeveloper* d = dynamic_cast<SoftwareDeveloper*>(records_[i])) lname = d->lastName();
 
+            if (lname == surname) {
+                cout << records_[i]->describe() << endl;
+                found = true;
+            }
+        }
+        if (!found) cout << "No person found with surname " << surname << endl;
+    }
+
+    // Шукає та виводить інфу про студентів з ідеальною вагою
+    void PrintIdealWeightStudents() const {
+        bool found = false;
+        cout << "Students with ideal weight:" << endl;
+        for (int i = 0; i < count_; ++i) {
+            if (Student* s = dynamic_cast<Student*>(records_[i])) {
+                if (s->isIdealWeight()) {
+                    cout << s->describe() << endl;
+                    found = true;
+                }
+            }
+        }
+        if (!found) cout << "No students with ideal weight found." << endl;
+    }
+
+    // Видалення інфи за фамілією
+    void DeleteBySurname(const string& surname) {
+        int newCount = 0;
+        for (int i = 0; i < count_; ++i) {
+            string lname;
+            if (Student* s = dynamic_cast<Student*>(records_[i])) lname = s->lastName();
+            else if (Librarian* l = dynamic_cast<Librarian*>(records_[i])) lname = l->lastName();
+            else if (SoftwareDeveloper* d = dynamic_cast<SoftwareDeveloper*>(records_[i])) lname = d->lastName();
+
+            if (lname != surname) {
+                records_[newCount++] = records_[i];
+            } else {
+                delete records_[i];
+            }
+        }
+        count_ = newCount;
+    }
+
+    // Зберігає всі введені дані в файл
+    bool SaveAll(const string& filename) {
+        ofstream ofs(filename, ios::out | ios::trunc);
+        if (!ofs.is_open()) return false;
+
+        for (int i = 0; i < count_; ++i) {
+            Record* r = records_[i];
+            if (!r) continue;
+            ofs << r->typeName() << " " << r->objectName() << "\n";
+            ofs << "{\n";
+            ofs << r->serializeAttributes() << "\n";
+            ofs << "};\n\n";
+        }
+        ofs.close();
+        return true;
+    }
+
+    // Функція для завантаження інфи до масиву
+    int LoadAll(const string& filename) {
+        ifstream ifs(filename);
+        if (!ifs.is_open()) return 0;
+
+        string line;
+        count_ = 0;
+        while (count_ < 100 && getline(ifs, line)) { // Читає всі рядки по порядку з лімітом в 100
+            if (line.find_first_not_of(" \t\r\n") == string::npos) continue;
+
+            // Регулювання для заголовку (ім'я фамілія персони)
+            smatch m;
+            regex headerRx(R"(^\s*([A-Za-z]+)\s+([A-Za-z0-9_]+)\s*$)");
+            if (!regex_match(line, m, headerRx)) continue;
+            string type = m[1];
+            string objName = m[2];
+
+            string block;
+            while (getline(ifs, line)) {
+                block += line + "\n";
+                if (line.find('}') != string::npos) break;
+            }
+
+            // регулювання "key": "value"
+            regex kv("\"([^\"]+)\":\\s*\"([^\"]*)\"");
+            sregex_iterator it(block.begin(), block.end(), kv);
+            sregex_iterator end;
+
+            string firstname, lastname;
+            string heightStr, weightStr, studentId, passport;
+            string employeeId, devId, lang;
+
+            // Парсер для "key": "value", заповнює атрибути інфою
+            for (; it != end; ++it) {
+                string key = (*it)[1];
+                string val = (*it)[2];
+                if (key == "firstname") firstname = val;
+                else if (key == "lastname") lastname = val;
+                else if (key == "height") heightStr = val;
+                else if (key == "weight") weightStr = val;
+                else if (key == "studentId") studentId = val;
+                else if (key == "passport") passport = val;
+                else if (key == "employeeId") employeeId = val;
+                else if (key == "devId") devId = val;
+                else if (key == "language") lang = val;
+            }
+
+            if (type == "Student") {
+                int h = heightStr.empty() ? 0 : stoi(heightStr); // Якщо висота або вага відсутні, виставляє 0
+                int w = weightStr.empty() ? 0 : stoi(weightStr);
+
+                // Створює об'єкт, конструктор приймає дані і записує їх в масив
+                records_[count_++] = new Student(firstname, lastname, h, w, studentId, passport);
+            } else if (type == "Librarian") {
+                records_[count_++] = new Librarian(firstname, lastname, employeeId);
+            } else if (type == "SoftwareDeveloper") {
+                records_[count_++] = new SoftwareDeveloper(firstname, lastname, devId, lang);
+            }
+        }
+
+        ifs.close();
+        return count_;
+    }
 };
